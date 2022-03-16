12c8d1fac0ee88c620f1e949377a5aee572c52a2
Dominik <36802235+Domepo@users.noreply.github.com>
2022-03-16 18:06:52 +0100

    Update passbild.py

--- src/passbild.py
+++ src/passbild.py
@@ -2,12 +2,17 @@ from __future__ import print_function
 import os
 from PIL import Image
 bilder = []
-def schleife(bilderinschleife,input):
+def schleife(bilderinschleife,inputa):
+    bilder.clear()
     counter = 0
-    eingabe = input
+    eingabe = inputa
     while counter < bilderinschleife:
         bilder.extend([eingabe])
         counter = counter +1
+    print(len(bilder))
+
+
+
 def bild(x,y,r,g,b):
     global result
     result = Image.new("RGBA", (x, y),(r, g, b))
@@ -20,11 +25,12 @@ def bildperso(thumbnailx,thumbnaily,xi,yi):
          x = index // 2 * xi
          y = index % 2 * yi
          w, h = img.size
-         print('pos {0},{1} size {2},{3}'.format(x, y, w, h))
+        #print('pos {0},{1} size {2},{3}'.format(x, y, w, h))
          result.paste(img, (x, y, x + w, y + h))
     #result.save(os.path.expanduser('1.jpg'))
 
 def bildfinal(x,y,r,g,b,xa,ya,save):
     final = Image.new("RGB",(x,y),(r, g, b))
     final.paste(result,(xa,ya))
-    final.save(save+".png")
+    sp = save.split(".")
+    final.save(sp[0]+" P"+".png")
